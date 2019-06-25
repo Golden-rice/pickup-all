d11bf0056a4ea166012773e5387276b0146cbebe
dongmin <simon-dong@qq.com>
2019-06-25 09:59:20 +0800

    重新写了Peact 对 class 与 element 的支持

--- Peact/index.js
+++ Peact/index.js
@@ -1,50 +1,13 @@
-
-// Peact 实现
-const Peact = {
-  /**
-   * 创造 Peact element 
-   * @param {*} type      (String) dom 类型
-   * @param {*} props     (Object) Peact element 属性
-   * @param {*} children  (Peact element or Dom or Basic type) 子节点 
-   */
-  createElement(type, props, children){ 
-    /* create a Peact element */ 
-    const element = {
-      type,
-      __type: "PeactElement",
-      props: props || {}
-    };
-
-    if (children) {
-      element.props.children = children;
-    }
-    return element;
-  },
-  createClass({ render = function(){} }){
-    // create a Peact class
-    function ElementClass(props){
-      this.props = props
-      this.__type = 'PeactClass'
-    }
-    // extend super render function
-    if( !!ElementClass.prototype.render ){
-      console.error("Required render function!")
-      return {};
-    }
-    ElementClass.prototype.render = render;
-    return ElementClass
-  },
-  render(element /* Peact class or Peact element */, container){
-    let componentInstance = {}
-    // 类型判断，最后均转化成 Peact element
-    if( element.__type && element.__type === "PeactClass"){
-      componentInstance = new PeactCompositeComponentWrapper(element);
-    }else{
-      componentInstance = new PeactDOMComponent(element);
-    }
-    return componentInstance.mountComponent(container);
-  }
-}
+/**
+ * 高阶函数
+ * 
+ */
+const TopLevelWrapper = function(props) {
+  this.props = props;
+};
+TopLevelWrapper.prototype.render = function() {
+  return this.props;
+};
 
 // Peact 基础组件
 class PeactDOMComponent {
@@ -66,26 +29,73 @@ class PeactDOMComponent {
   }
 }
 
-// class wrapper，转化成 Peact element
+/**
+ * 对 Peact 封装的高阶组件进行计算
+ */
 class PeactCompositeComponentWrapper {
-  constructor(c) {
-      this._currentElement = c;
+  constructor(element) {
+    this._currentElement = element;
   }
+
   mountComponent(container) {
-      const elementClass = this._currentElement;
-      if( !this._currentElement.props ){
-        console.error("Required props!")
-        return {}
-      }
+    // Component 就是 TopLevelWrapper 构造函数
+    const Component = this._currentElement.type;
+    // this._currentElement.props 就是 Peact.render() 第一个参数
+    const componentInstance = new Component(this._currentElement.props);
+    // 如果是 Peact element，则 element 是 Peact.render() 第一个参数，如果是 Peact class，element 就是 Peact.createClass 内部的构造函数 ElementClass
+    let element = componentInstance.render();
+    // 对 element 类型判断决定，如果是 Peact class 则element 是构造函数，如果是 Peact element，element 是string，
+    while (typeof element === 'function') {
+      // render 为 Peact class 声明时的 render 
+      element = (new element(element.props)).render();
+    }
 
-      const element = elementClass.render();
-      // element rely on render, so must judge element type
-      if( !element.__type || element.__type !== "PeactElement" ){
-        console.error("Render create not a Peact element!")
-        return {}
-      }
+    // 确保此处的 element 为 Peact element
+    const domComponentInstance = new PeactDOMComponent(element);
+    domComponentInstance.mountComponent(container);
+  }
+}
 
-      const domComponentInstance = new PeactDOMComponent(element);
-      return domComponentInstance.mountComponent(container);
+// Peact 实现
+const Peact = {
+  /**
+   * 创造 Peact element 
+   * @param {*} type      (String) dom 类型
+   * @param {*} props     (Object) Peact element 属性
+   * @param {*} children  (Peact element or Dom or Basic type) 子节点 
+   */
+  createElement(type, props, children){ 
+    /* create a Peact element */ 
+    const element = {
+      type,
+      props: props || {}
+    };
+
+    if (children) {
+      element.props.children = children;
+    }
+    return element;
+  },
+  createClass({ render }){
+    // create a Peact class
+    function ElementClass(props){
+      this.props = props
+    }
+    if( !render ){
+      console.error("Required render function!")
+      return {};
+    }
+    ElementClass.prototype.render = render;
+    return ElementClass
+  },
+  render(element /* Peact class or Peact element */, container){
+    // wrapperElement 包含 type 为构造函数，props为组件 element，children = null，prototype.render 函数，
+    const wrapperElement = this.createElement(TopLevelWrapper, element);
+    // 利用 type 执行构造
+    // 将 element 赋值给 this.props (此处的element 可能是 Peact element，也可能是 Peact class)
+    // 执行 prototype.render() 返回 this.props
+    // 如果是 Peact element， PeactDOMComponent() 
+    const componentInstance = new PeactCompositeComponentWrapper(wrapperElement);
+    return componentInstance.mountComponent(container);
   }
 }
